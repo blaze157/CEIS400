8321f5b4d8274c8748716cfdb31a01bdba0e6a73
GoopyDinoEggDrop <AustinBabineau@gmail.com>
2017-12-01 19:00:51 -0800

    Update Employee.cpp
    Implemented checkInEquipment(), checkOutEquipment(), inputValid()
    TODO: Implement IDInUse() // Need database access to do so

--- Employee.cpp
+++ Employee.cpp
@@ -29,17 +29,30 @@ void Employee::setSkillClass(int sc)
 {
 	skillClass = sc;
 }
-void Employee::printEquipment()
+std::string Employee::printEquipment()
 {
-
+	std::string output;
+	for (int i = 0; i < equipment.size; i++)
+	{
+		output.append(equipment.at(i).getName());
+		output.append(" , ");
+	}
+	return output;
 }
-void Employee::checkOutEquipment()
+void Employee::checkOutEquipment(Equipment e)
 {
-
+	e.setID(ID);
+	equipment.push_back(e);
 }
-void Employee::checkInEquipment()
+void Employee::checkInEquipment(Equipment e)
 {
-
+	for (int i = 0; i < equipment.size; i++)
+	{
+		if (equipment.at(i).getName() == e.getName())
+		{
+			equipment.erase(equipment.begin()+i);
+		}
+	}	
 }
 bool Employee::IDInUse(int id)
 {
@@ -49,5 +62,34 @@ bool Employee::IDInUse(int id)
 bool Employee::inputValid(int id, std::string pass)
 {
 	//call IDInUse(id) , check if password meets standards (standards tbd)
+	if (!IDInUse(id))
+	{
+		if (pass.length > 8)
+		{
+			for (int i = 0; i < pass.length; i++)
+			{
+				if (isdigit(pass.at(i)))
+				{
+					for (int j = 0; j < pass.length; j++)
+					{
+						if (isLetter(pass.at(j)))
+							return true;
+					}
+				}
+			}
+		}
+	}
+	return false;
+}
+bool isLetter(char c)
+{
+	if (toupper(c) == 'A' || toupper(c) == 'B' || toupper(c) == 'C' || toupper(c) == 'D' ||
+		toupper(c) == 'E' || toupper(c) == 'F' || toupper(c) == 'G' || toupper(c) == 'H' ||
+		toupper(c) == 'I' || toupper(c) == 'J' || toupper(c) == 'K' || toupper(c) == 'L' ||
+		toupper(c) == 'M' || toupper(c) == 'N' || toupper(c) == 'O' || toupper(c) == 'P' ||
+		toupper(c) == 'Q' || toupper(c) == 'R' || toupper(c) == 'S' || toupper(c) == 'T' ||
+		toupper(c) == 'U' || toupper(c) == 'V' || toupper(c) == 'W' || toupper(c) == 'X' ||
+		toupper(c) == 'Y' || toupper(c) == 'Z')
+		return true;
 	return false;
 }
