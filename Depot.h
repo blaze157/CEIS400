65134b829df80e529dfac5355f2b466cfc7f736e
GoopyDinoEggDrop <AustinBabineau@gmail.com>
2017-12-03 15:19:12 -0800

    Update Depot.h

--- Depot.h
+++ Depot.h
@@ -1,14 +1,17 @@
 #include <string>
+//#include "Equipment.h"
+#include <vector>
 class Depot
 {
 public:
 	Depot();
 	void orderEquipment(int, std::string); //id,name
-	bool checkInventory(std::string);//equipment name
-	void removeEquipment(std::string);
-	void addEquipment(std::string);
+	bool checkInventory(int);//equipment id
+	void removeEquipment(int);//id
+	void addEquipment(int);//id
 	void printInventory();
 	void notifyEmployee();
 private:
 	//database
+	//std::vector<Equipment> equipment;
 };
